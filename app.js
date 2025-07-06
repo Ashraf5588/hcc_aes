4e943677cd90cf46e4eb9e3cd5262a51789a13b6
Ashraf5588 <ashraf653@hsmonline.edu.np>
2025-07-06 06:16:56 +0545

    port changed to 80

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ connection();
 
 app.use(student)
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
+app.listen(80,()=>{
+    console.log('Server is running on port 80')
 
 })
